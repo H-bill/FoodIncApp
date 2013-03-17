19a517fa06d781c56e6356e9c09d5b4cfd1cd16f
Rafael Dalibera <rafaeldalibera@gmail.com>
2013-03-17 08:45:40 -0400

    Modifications on the Activity that displays the recipes

--- FoodApp_BILL_ANDROID/src/com/example/foodapp/DisplayRecipe.java
+++ FoodApp_BILL_ANDROID/src/com/example/foodapp/DisplayRecipe.java
@@ -2,56 +2,85 @@ package com.example.foodapp;
 
 import java.util.Iterator;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ShareActionProvider;
 import android.widget.Toast;
 
 public class DisplayRecipe extends Activity {
 	
 	WebView mWebView;
+	String url;
+	ShareActionProvider provider;
+	String TAG;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.display_recipe);
+        
+        TAG = "MyActivity2";
+        
+        final String TAG = "MyActivity2";
+        url = "http://www.google.com";
         
         Intent intent = getIntent();
         String recipe = intent.getExtras().getString("recipe");
-        
-        mWebView = (WebView) findViewById (R.id.webView);
-        
-        mWebView.loadUrl("file:///android_asset/untitled.html");
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.getSettings().setSaveFormData(true);
-        mWebView.getSettings().setBuiltInZoomControls(true);
-        mWebView.setWebViewClient(new MyWebViewClient());
-        
+
+        WebView wv = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = wv.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        wv.loadUrl(url);
     }
 	
+	@SuppressLint("NewApi")
 	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.share_menu, menu);
+	    getMenuInflater().inflate(R.menu.share_menu, menu);
+
+	    // Get the ActionProvider
+	    provider = (ShareActionProvider) menu.findItem(R.id.share_button)
+	        .getActionProvider();
+	    // Initialize the share intent
+	    Intent intent = new Intent(Intent.ACTION_SEND);
+	    intent.setType("text/plain");
+	    provider.setShareIntent(intent);
         return true;
     }
 	
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int duration = Toast.LENGTH_SHORT;
-    	Context context = getApplicationContext();
-        	        	
-	    Toast toast = Toast.makeText(context, "search", duration);
-	    toast.show();			
-		
-		
-    	return true;
-	}
+	  public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    case R.id.share_button:
+	      Log.i(TAG, "okay1");
+	      doShare();
+	      break;
+	    default:
+	      break;
+	    }
+	    return true;
+	  }
+	
+	public void doShare() {
+	    // Populare the share intent with data
+		Log.i(TAG, "okay2");
+	    Intent intent = new Intent(Intent.ACTION_SEND);
+	    intent.setType("text/plain");
+	    intent.putExtra(Intent.EXTRA_TEXT, "This is a message for you");
+	    provider.setShareIntent(intent);
+	  } 
+	
+	
 
 }
 
